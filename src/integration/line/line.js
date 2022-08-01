63d5b112508fde81657db1577b9caa7e142ae7a3
cherylbraus <cherylbraus@transfix.io>
2022-08-01 17:36:50 -0400

    updated line plot tooltips for looker

--- src/integration/line/line.js
+++ src/integration/line/line.js
@@ -30,17 +30,6 @@ looker.plugins.visualizations.add({
           ],
           default: "dollar"
         },
-        index_start: {
-          type: 'string',
-          label: 'Index Option',
-          display: "radio",
-          values: [
-            {"one": "0"},
-            {"two": "1"},
-            {"three": "2"}
-          ],
-          default: "0"
-        }
       },
 
     // Set up the initial state of the visualization
@@ -98,7 +87,30 @@ looker.plugins.visualizations.add({
         }
     
     try {
-        console.log(config)
+    // console.log(config)
+
+    // first we set the dimensions and margins of the graphic
+    const margin = {top: 40, right: 20, bottom: 66, left: 60};
+    const width = element.clientWidth - margin.left - margin.right;
+    const height = element.clientHeight - margin.top - margin.bottom;
+
+    console.log("element", element)
+
+    const svg = (
+      d3.select(element).select('svg')
+        .html('')
+        .attr('width', '100%')
+        .attr('height', '100%')
+    )
+
+    const group = svg.append('g')
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
+        .attr("width", "100%")
+        .attr("height", (height + "px"))
+        .classed("group", true)
+
+
+    // determine which parseTime to use by looking at the data columns
     var parseTimeDay = d3.timeParse("%Y-%m-%d");
     var parseTimeMonth = d3.timeParse("%Y-%m");
 
@@ -107,29 +119,41 @@ looker.plugins.visualizations.add({
     const dimensions_ordered = queryResponse.fields.dimension_like
     const measures_ordered = queryResponse.fields.measure_like
 
+    // determine if parseTime should be by day or by month
     if (parseTimeDay(data[1][dimensions_ordered[0].name].value)) {
       parseTime = parseTimeDay
     } else {
       parseTime = parseTimeMonth
     }
+
+    data.forEach(function(d) {
+      if (parseTime(d[dimensions_ordered[0].name].value) == null) {
+        return 
+      }
+      d[dimensions_ordered[0].name].value = parseTime(d[dimensions_ordered[0].name].value)
+    })
+
+    data.sort(function(a,b) {
+      return b.date - a.date
+    })
+
+    console.log("DATA", data)
+
+
+    // divide the data into 3 separate groups
     const colors = ["#27566b", "#007b82", "#8cbb61"]
-    const index_options = ["0","3","12"]
+
     let data1 = []
     let data2 = []
     let data3 = []
 
-    index_options.forEach(function(j, index) {
+    measures_ordered.forEach(function(j, index) {
       data.forEach(function(d) {
 
         const data_pod = {};
-        if (parseTime(d[dimensions_ordered[0].name].value) == null) {
-          return
-        }
-        const num = +config.index_start + (+j)
 
-        data_pod["date"] = parseTime(d[dimensions_ordered[0].name].value);
-
-        data_pod["value"] = d[measures_ordered[num].name].value;
+        data_pod["date"] = d[dimensions_ordered[0].name].value;
+        data_pod["value"] = d[measures_ordered[index].name].value;
 
         if (index == 0) {
           data1.push(data_pod)
@@ -141,32 +165,61 @@ looker.plugins.visualizations.add({
       })
     })
 
-    data1.sort(function(a,b){
-      return b.date - a.date
-    })
 
-    data2.sort(function(a,b){
-      return b.date - a.date
-    })
-
-    data3.sort(function(a,b){
-      return b.date - a.date
-    })
-    // console.log(data1, data2, data3)
-    // first we set the dimensions and margins of the graphic
-        const margin = {top: 40, right: 20, bottom: 66, left: 60};
-        const width = element.clientWidth - margin.left - margin.right;
-        const height = element.clientHeight - margin.top - margin.bottom;
-
+    console.log("data1, data2, data3", data1, data2, data3)
+    
     // parseTime becomes a function using the d3.timeParse() function, it's to make the dates
     // in our csv file readable to our machine
 
-    var x = d3.scaleTime().range([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d[dimensions_ordered[0].name].value; }))
+      .range([0, width]);
+
+    const minVal = Math.min(
+      Math.min(...data1.map(d => d.value)), 
+      Math.min(...data2.map(d => d.value)),
+      Math.min(...data3.map(d => d.value))
+      )
+
+    const maxVal = Math.max(
+      Math.max(...data1.map(d => d.value)), 
+      Math.max(...data2.map(d => d.value)),
+      Math.max(...data3.map(d => d.value))
+      )
+    
+    var y = d3.scaleLinear()
+      .domain([minVal, maxVal])
+      .range([height, 0]);
+
+    // -----------------------------
+    // NEW DATE LOGIC - DIDN'T USE CLIPPATHS 
+    const today = new Date()
+
+    let dateCutoff;
+
+    data3.reverse().forEach(function(j, index) {
+        if (j.date < today) {
+            dateCutoff = j.date
+        } else {
+            return
+        }
+    })
+
+    // console.log("dateCutoff", dateCutoff)
+
+    data3 = data3.filter(d => d.date < dateCutoff) // actuals
+    data2 = data2.filter(d => d.date >= d3.max(data3, d => d.date)) // forecast
+
+    console.log("cutoff data2, data3", data2, data3)
+
+
+    // ------------------------------
+
+
 
     var line = d3.line()
           .defined(function(d) { return d.value != null && d.value != 0 && x(d.date) >= 0})
-          .curve(d3.curveBasis)
+          .curve(d3.curveNatural)
           .x(function(d) {
             return x(d.date);})
           .y(function(d) {
@@ -175,7 +228,7 @@ looker.plugins.visualizations.add({
 
     var movingLine = d3.line()
           .defined(function(d) { return d.value != null && x(d.date) >= 0})
-          .curve(d3.curveBasis)
+          .curve(d3.curveNatural)
           .x(function(d) {
             return x(d.date);})
           .y(function(d) {
@@ -210,19 +263,7 @@ looker.plugins.visualizations.add({
       movingData.push(data_pod)
     })
 
-        const svg = (
-          d3.select(element).select('svg')
-            .html('')
-            .attr('width', '100%')
-            .attr('height', '100%')
-        )
-
-        const group = svg.append('g')
-            .attr("transform", `translate(${margin.left}, ${margin.top})`)
-            .attr("width", "100%")
-            .attr("height", ((height - margin.top - margin.bottom) + "px"))
-                .classed("group", true)
-
+    // console.log("successfully created moving avg line")
 
     // The d3.csv function gets the data and returns it, we iterate over each part of that
     // data object to transform it into machine-readable data
@@ -255,6 +296,7 @@ looker.plugins.visualizations.add({
             .attr("d", movingLine);
 
           rawLine.attr("class", "line");
+
       } else {
         const newRawLine = group.append("path")
           .data([data3])
@@ -262,9 +304,12 @@ looker.plugins.visualizations.add({
           .attr("d", line);
 
       }
+
+      // console.log("drew lines")
+
       var legend = group.selectAll(".legend")
         .data(colors)
-      .enter()
+        .enter()
         .append("g")
           .attr("class", "legend")
           .attr("transform", function(d, i) { return "translate(" + (-100 + (i * 110) - width/2) + "," + (height + margin.bottom/2.3) + ")"; });
@@ -340,6 +385,8 @@ looker.plugins.visualizations.add({
                   return arr[i]
                   });
 
+      // console.log("drew legend")
+
       // Add the x Axis
       group.append("g")
           .attr("transform", "translate(0," + height + ")")
@@ -368,6 +415,150 @@ looker.plugins.visualizations.add({
 
       }
 
+      // -----------------------------------------------------------------------------
+      // TOOLTIPS
+
+      // tooltip div outside of svg to house the information and form the tooltip text box
+      const tooltip = d3.select(element)
+        .append("div")
+        .attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("padding", "5px")
+        .style("background-color", "#ffffff")
+        .style("border", "solid")
+        .style("border-color", "lightgrey")
+        .style("border-width", ".5px")
+        .attr("class", "tooltip")
+  
+      // group element to house all the tooltip things in svg
+      const tt = group.append("g")
+        .classed("tooltip", true)
+
+      const tooltipLine = tt.append("line")
+  
+      const tooltipCircles = tt.append("g")
+
+      // box that covers entire plot area and is see-through
+      const tooltipBox = tt.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("opacity", 0)
+        .on("mousemove", drawTooltip)
+        .on("mouseout", removeTooltip)
+
+      // calculate dates
+      const startAndEndWeek = (date) => {
+        const dt = new Date(date)
+        const monday = new Date(dt)
+        monday.setDate(monday.getDate() - monday.getDay() + 1)
+        const nextMonday = new Date(dt)
+        nextMonday.setDate(nextMonday.getDate() - nextMonday.getDay() + 8)
+        return [monday, nextMonday]
+      }
+
+      function removeTooltip() {
+        if (tooltip) tooltip.style("display", "none");
+        if (tooltipLine) tooltipLine.attr("stroke", "none");
+        tooltipCircles.selectAll("circle").remove();
+      }
+
+      function drawTooltip() {
+
+        // data wrangling - determine the date of the mouse
+        const ttdate = x.invert(d3.mouse(tooltipBox.node())[0])
+        const ttdayofwk = d3.timeFormat("%u")(ttdate)
+
+        const [mon, nextMon] = startAndEndWeek(ttdate)
+
+        // calculate the closest monday date to the mouse date since data is weekly
+        const closestMonday = function(ttdayofwk) {
+          if (["5","6"].includes(ttdayofwk)) {
+            const wkdt = nextMon;
+            return wkdt
+          } else {
+            const wkdt = mon;
+            return wkdt
+          }
+        } 
+
+        // set wkdt to not have a time so can filter the data on that date correctly
+        const wkdt = closestMonday(ttdayofwk).setHours(0,0,0,0)
+
+        // reformat data so that can easily filter on it for the tooltip data
+        const tooltipData = [{"name":"target", "info": data1}, {"name":"forecast", "info":data2}, {"name":"actual", "info":data3}]
+
+        const ttdata = []
+
+        tooltipData.forEach(d => {
+          const filtered = d.info.filter(i => {
+            return i.date.getTime() == wkdt
+          })
+
+          if (filtered.length > 0) {
+            ttdata.push({"name":d.name, "info":filtered})
+          }
+        })
+
+        ttdata.sort((a,b) => {
+          return b.info[0].value - a.info[0].value // does do something
+        })
+
+        // add elements/shapes
+        tooltipLine.attr("stroke", "#a6a6a6")
+          .attr("x1", x(wkdt))
+          .attr("x2", x(wkdt))
+          .attr("y1", 0)
+          .attr("y2", height)
+          .attr("stroke-dasharray", "3, 3")
+
+        console.log('TTDATA', ttdata)
+        console.log("x", x(wkdt), wkdt)
+        console.log(width, height)
+
+        tooltipCircles.selectAll("circle").remove();
+        tooltipCircles.selectAll("circle")
+          .data(ttdata)
+          .enter()
+          .append("circle")
+          .attr("r", 3)
+          .attr("cx", d => x(d.info[0].date))
+          .attr("cy", d => y(d.info[0].value))
+          .style('fill', '#3a4245')
+
+        // have title case for the labels in the tooltip text
+        function titleCase(str) {
+          let sentence = str.toLowerCase().split(" ");
+          for (let i = 0; i < sentence.length; i++) {
+            sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1)
+          }
+          return sentence.join(" ")
+        }
+
+        tooltip.html(d3.timeFormat("%b %-d, %Y")(wkdt) + "<hr>")
+          .style("display", "block")
+          .style("font-size", 12)
+          // .style("left", d3.event.pageX + 15 + "px")
+          .style("top", d3.event.pageY - 10 + "px")
+          .selectAll()
+          .data(ttdata)
+          .enter()
+          .append("div")
+          .style("color", "#323232")
+          .style("font-size", 12)
+          .html(d => titleCase(d.name) + ':&nbsp' + '<span style="float:right;">' + d3.format(",.0f")(d.info[0].value) + '</span>')
+
+        // if mouse is past 85% of x width, move the tooltip text box to the left of the line
+        if (d3.event.pageX > width*.85) {
+          tooltip
+            .style("left", d3.event.pageX - 110 + "px")
+        } else {
+          tooltip
+            .style("left", d3.event.pageX + 15 + "px")
+        }
+      }
+
+      // -----------------------------------------------------------------------------
+
         } catch(error) {
             if (environment == "prod") {
                 if (queryResponse.fields.dimensions.length != queryResponse.fields.measures.length) {
@@ -379,7 +570,6 @@ looker.plugins.visualizations.add({
             }
             
         }
-
 
         // Callback at the end of the rendering to let Looker know it's finished
         done()
--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,11 @@ var webpackConfig = {
     // bulletChart: './src/integration/bulletChart/bulletChart.js',
     // coloredBars: './src/integration/coloredBars/coloredBars.js',
     // coloredBarsPerc: './src/integration/coloredBarsPerc/coloredBarsPerc.js',
-    // line: './src/integration/line/line.js',
+    line: './src/integration/line/line.js',
     // sparklines: './src/integration/sparklines/sparklines.js',
     // fever: './src/integration/fever/fever.js'
     // coloredBar: './src/integration/coloredBar/coloredBar.js',
-    template: './src/integration/template/template.js',
+    // template: './src/integration/template/template.js',
   },
   output: {
     filename: "[name].js",
