be6b19058dbb6ca928ae9b9d3294f45cbf3a24a0
cherylbraus <cherylbraus@transfix.io>
2022-08-16 15:12:34 -0400

    2 measures and option functionality to colored bar

--- src/integration/template/template.js
+++ src/integration/template/template.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3'
 import * as d3Collection from 'd3-collection'
 import { formatType, handleErrors } from '../common/utils'
-import { object } from './splitViolin'
+import { object } from './coloredBar'
 import * as $ from 'jquery'
 
 // Query the element
@@ -100,7 +100,7 @@ keys.forEach(function(entry, i) {
 })
 
 
-d3.json("http://localhost:3001/dataSplitViolinRegion").then(function(data) {
+d3.json("http://localhost:3001/dataColoredBarTwo").then(function(data) {
 	let todays_options = {}
 
 	$('input:radio:checked').each(function() {
